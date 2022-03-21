8215281517c4c09b5e8ecd9c68466b82641c5107
Alex Chinedu <nwekealexchinedu@gmail.com>
2022-03-21 22:25:31 +0100

    started working on the sliver app bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,30 @@
+import 'package:facebook_clone_ui/config/palette.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            systemOverlayStyle: SystemUiOverlayStyle.dark,
+            backgroundColor: Colors.white,
+            title: const Text(
+              'facebook',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                letterSpacing: -1.2,
+                fontSize: 28.0,
+                color: Palette.facebookBlue,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
