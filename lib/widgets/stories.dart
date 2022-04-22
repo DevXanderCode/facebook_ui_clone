e2cf5221f964aea4e716f89af9803e39f43aef33
Alex Chinedu <nwekealexchinedu@gmail.com>
2022-04-22 19:36:40 +0100

    started on the private story card widget

--- lib/widgets/stories.dart
+++ lib/widgets/stories.dart
@@ -3,13 +3,55 @@ import 'package:facebook_clone_ui/models/models.dart';
 
 class Stories extends StatelessWidget {
   final User currentUser;
-  final Post stories;
+  final List<Story> stories;
 
   const Stories({
     Key? key,
     required this.currentUser,
     required this.stories,
   }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200.0,
+      color: Colors.purple,
+      child: ListView.builder(
+        padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 8.0),
+        scrollDirection: Axis.horizontal,
+        itemCount: 1 + stories.length,
+        itemBuilder: (BuildContext context, index) {
+          if (index == 0) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: _StoryCard(
+                isAddStory: true,
+                currentUser: currentUser,
+              ),
+            );
+          }
+
+          final Story story = stories[index - 1];
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 4.0),
+            child: _StoryCard(
+              story: story,
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _StoryCard extends StatelessWidget {
+  final bool isAddStory;
+  final User currentUser;
+  final Story story;
+
+  const _StoryCard(
+      {Key? key, this.isAddStory = false, this.currentUser, this.story})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
