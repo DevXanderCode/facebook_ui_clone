e4e4ce7a7290bc03ff4662dde71549847b214aba
Alex Chinedu <nwekealexchinedu@gmail.com>
2022-04-22 08:20:51 +0100

    finished working on the top section of the create post container

--- lib/widgets/create_post_container.dart
+++ lib/widgets/create_post_container.dart
@@ -10,18 +10,36 @@ class CreatePostContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.fromLTRB(12.0, 8.0, 12.0, 0.0),
       color: Colors.orange,
-      child: Column(children: [
-        Row(
-          children: [
-            CircleAvatar(
-              radius: 20.0,
-              backgroundColor: Colors.grey[200],
-              backgroundImage: CachedNetworkImageProvider(currentUser.imageUrl),
-            )
-          ],
-        )
-      ]),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              CircleAvatar(
+                radius: 20.0,
+                backgroundColor: Colors.grey[200],
+                backgroundImage:
+                    CachedNetworkImageProvider(currentUser.imageUrl),
+              ),
+              const SizedBox(
+                width: 8.0,
+              ),
+              const Expanded(
+                child: TextField(
+                  decoration: InputDecoration.collapsed(
+                    hintText: 'What\'s on your mind ?',
+                  ),
+                ),
+              )
+            ],
+          ),
+          const Divider(
+            height: 10.0,
+            thickness: 0.5,
+          ),
+        ],
+      ),
     );
   }
 }
