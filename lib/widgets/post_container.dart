242d90c9a520d0759cf9ffcacf460bb2be93836b
Alex Chinedu <nwekealexchinedu@gmail.com>
2022-04-23 13:17:33 +0100

    started working on the private psot header widget

--- lib/widgets/post_container.dart
+++ lib/widgets/post_container.dart
@@ -1,3 +1,4 @@
+import 'package:facebook_clone_ui/widgets/profile_avatar.dart';
 import 'package:flutter/material.dart';
 import 'package:facebook_clone_ui/models/models.dart';
 
@@ -10,6 +11,55 @@ class PostContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 5.0),
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      color: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            _PostHeader(post: post),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Text(post.caption),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _PostHeader extends StatelessWidget {
+  final Post post;
+  const _PostHeader({
+    Key? key,
+    required this.post,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          children: [
+            ProfileAvatar(imageUrl: post.user.imageUrl),
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Text(post.user.name),
+            )
+          ],
+        ),
+        IconButton(
+          onPressed: () => print('more'),
+          icon: const Icon(Icons.more_horiz_rounded),
+          iconSize: 25.0,
+        ),
+      ],
+    );
   }
 }
