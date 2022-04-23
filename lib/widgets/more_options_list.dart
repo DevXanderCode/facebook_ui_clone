a0667c068b8ada1c13006f5e87bd3c52dba8640a
Alex Chinedu <nwekealexchinedu@gmail.com>
2022-04-24 00:37:23 +0100

    started working on the option custom widget

--- lib/widgets/more_options_list.dart
+++ lib/widgets/more_options_list.dart
@@ -1,15 +1,61 @@
+import 'package:facebook_clone_ui/config/palette.dart';
 import 'package:facebook_clone_ui/models/user_model.dart';
+import 'package:facebook_clone_ui/widgets/user_card.dart';
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class MoreOptionsList extends StatelessWidget {
   final List<List> _moreOptionsList = const [
-    [MdiIcons.shieldAccount, Colors.deepPurple, 'Covid 19 Info Center']
+    [MdiIcons.shieldAccount, Colors.deepPurple, 'Covid 19 Info Center'],
+    [MdiIcons.accountMinusOutline, Colors.cyan, 'Friends'],
+    [MdiIcons.facebookMessenger, Palette.facebookBlue, 'Messanger'],
+    [MdiIcons.flag, Colors.orange, 'Pages'],
+    [MdiIcons.storefront, Palette.facebookBlue, 'Marketplace'],
+    [Icons.ondemand_video, Palette.facebookBlue, 'Watch'],
+    [MdiIcons.calendarStar, Colors.red, 'Events'],
   ];
   final User currentUser;
   const MoreOptionsList({
     Key? key,
     required this.currentUser,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: BoxConstraints(maxWidth: 280.0),
+      child: ListView.builder(
+          itemCount: 1 + _moreOptionsList.length,
+          itemBuilder: (BuildContext context, int index) {
+            if (index == 0) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: UserCard(user: currentUser),
+              );
+            }
+            final List option = _moreOptionsList[index - 1];
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: _Option(
+                icon: option[0],
+                color: option[1],
+                label: option[2],
+              ),
+            );
+          }),
+    );
+  }
+}
+
+class _Option extends StatelessWidget {
+  final IconData icon;
+  final Color color;
+  final String label;
+  const _Option({
+    Key? key,
+    required this.icon,
+    required this.color,
+    required this.label,
   }) : super(key: key);
 
   @override
