d8edc8b3ecc8e00da0d9652b9cb9a942718f0717
Alex Chinedu <nwekealexchinedu@gmail.com>
2022-04-23 23:59:54 +0100

    added the tracking scroll controller to the home screen for both mobile and Desktop

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,8 +6,22 @@ import 'package:material_design_icons_flutter/material_design_icons_flutter.dart
 import 'package:facebook_clone_ui/data/data.dart';
 import 'package:facebook_clone_ui/models/models.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final TrackingScrollController _trackingScrollController =
+      TrackingScrollController();
+
+  @override
+  void dispose() {
+    _trackingScrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,8 +29,10 @@ class HomeScreen extends StatelessWidget {
       onTap: () => FocusScope.of(context).unfocus(),
       child: Scaffold(
         body: Responsive(
-          mobile: _HomeScreenMobile(),
-          desktop: _HomeScreenDesktop(),
+          mobile:
+              _HomeScreenMobile(scrollController: _trackingScrollController),
+          desktop:
+              _HomeScreenDesktop(scrollController: _trackingScrollController),
         ),
       ),
     );
@@ -24,11 +40,17 @@ class HomeScreen extends StatelessWidget {
 }
 
 class _HomeScreenMobile extends StatelessWidget {
-  const _HomeScreenMobile({Key? key}) : super(key: key);
+  final TrackingScrollController scrollController;
+
+  const _HomeScreenMobile({
+    Key? key,
+    required this.scrollController,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return CustomScrollView(
+      controller: scrollController,
       slivers: [
         SliverAppBar(
           systemOverlayStyle: SystemUiOverlayStyle.light,
@@ -92,7 +114,11 @@ class _HomeScreenMobile extends StatelessWidget {
 }
 
 class _HomeScreenDesktop extends StatelessWidget {
-  const _HomeScreenDesktop({Key? key}) : super(key: key);
+  final TrackingScrollController scrollController;
+  const _HomeScreenDesktop({
+    Key? key,
+    required this.scrollController,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -108,6 +134,7 @@ class _HomeScreenDesktop extends StatelessWidget {
         SizedBox(
             width: 600.0,
             child: CustomScrollView(
+              controller: scrollController,
               slivers: [
                 SliverPadding(
                   padding: const EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 5.0),
